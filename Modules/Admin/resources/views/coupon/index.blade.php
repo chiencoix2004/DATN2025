d9b6ec2d330b40c50a6af45c0659e824abb624de
xuyenqua <ntx01062004@gmail.com>
2024-10-11 20:49:18 +0700

    fix giao diện coupon

--- Modules/Admin/resources/views/coupon/index.blade.php
+++ Modules/Admin/resources/views/coupon/index.blade.php
@@ -13,27 +13,33 @@
 @section('css-setting')
 @endsection
 @section('contents')
-    <div class="container">
-        <a name="" id="" class="btn btn-primary" href="{{ route('admin.coupons.create') }}" role="button">Thêm
-            mới</a>
-        <table class="table table-bordered" id="example" class="display">
-            <thead>
-                <tr>
-                    <th>Mã</th> <!-- id -->
-                    <th>Tên</th> <!-- name -->
-                    <th>Mã giảm giá</th> <!-- code -->
-                    <th>giá trị giảm</th> <!-- discount_amount -->
-                    <th>Loại giảm giá</th> <!-- discount_type -->
-                    <th>Số lượng</th> <!-- quantity -->
-                    <th>Hành động</th> <!-- action -->
-                </tr>
-            </thead>
-            <tbody class="table-striped">
-                <tr>
+    <div class="container mt-4">
+        <div class="card mb-3 p-5">
 
-                </tr>
-            </tbody>
-        </table>
+            <div>
+                <a name="" id="" class="btn btn-primary" href="{{ route('admin.coupons.create') }}"
+                    role="button">Thêm
+                    mới</a>
+            </div>
+            <table class="table table-bordered" id="example" class="display">
+                <thead>
+                    <tr>
+                        <th>Mã</th> <!-- id -->
+                        <th>Tên</th> <!-- name -->
+                        <th>Mã giảm giá</th> <!-- code -->
+                        <th>giá trị giảm</th> <!-- discount_amount -->
+                        <th>Loại giảm giá</th> <!-- discount_type -->
+                        <th>Số lượng</th> <!-- quantity -->
+                        <th>Hành động</th> <!-- action -->
+                    </tr>
+                </thead>
+                <tbody class="table-striped">
+                    <tr>
+
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
     <script type="text/javascript">
         $(document).ready(function() {
