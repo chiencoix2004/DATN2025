ed30c9629d37aa1066701aacd18f4f36c97dcbe1
Binh01vn <bimhaybuon2k4@gmail.com>
2024-11-23 18:38:08 +0700

    Bình: cập nhật chi tiết sản phẩm client

--- Modules/Client/routes/web.php
+++ Modules/Client/routes/web.php
@@ -32,9 +32,9 @@ Route::controller(ClientController::class)->group(function () {
     Route::post('search', 'search')->name('search');
     Route::post('shortingseach', 'shortingseach')->name('shortingseach');
     Route::post('searchprice', 'searchprice')->name('searchprice');
-    route::get('seach/category/{id}/{keywd}','seachcategory')->name('seachcategory');
+    route::get('seach/category/{id}/{keywd}', 'seachcategory')->name('seachcategory');
     route::get(('search/{keywd}'), 'searchget')->name('searchget');
-   // Route::get('querybuilder')
+    // Route::get('querybuilder')
 });
 Route::controller(ShopController::class)->prefix('shop')->as('shop.')->group(function () {
     Route::get('/', 'index')->name('shopIndex');
@@ -52,11 +52,11 @@ Route::prefix('other')->as('other.')->group(function () {
     Route::get('aboutUs', function () {
         return view('client::contents.other-pages.about-us');
     })->name('aboutUs');
-  //Bài viết
-    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');      
+    //Bài viết
+    Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
     Route::get('/postDetail/{slug}', [PostController::class, 'show'])->name('postDetail');
     Route::post('search', [PostController::class, 'search'])->name('posts.search');
-});    
+});
 
 Route::controller(RegisterController::class)->prefix('auth')->as('auth.')->group(function () {
     Route::post('log-reg', 'register')->name('log-reg');
@@ -95,12 +95,14 @@ Route::get('auth/google', [LoginController::class, 'redirectToGoogle'])->name('a
 Route::get('auth/google/callback', [LoginController::class, 'handleGoogleCallback']);
 
 //giỏ hàng
-Route::post('/cart/add', [CartController::class, 'add'])->name('cart.add');
-Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
-Route::get('/cart/list', [CartController::class, 'list'])->name('cart.list');
-Route::put('/cart/update/{id}', [CartController::class, 'update'])->name('cart.update');
-Route::delete('/cart/remove/{id}', [CartController::class, 'remove'])->name('cart.remove');
-Route::post('/cart/apply-coupon', [CartController::class, 'applyCoupon'])->name('cart.applyCoupon');
-Route::get('/cart/checkout', [CartController::class, 'order'])->name('cart.checkout');
-Route::post('/cart/remove-discount-code', [CartController::class, 'removeDiscountCode'])->name('cart.removeDiscountCode');
 
+Route::controller(CartController::class)->group(function () {
+    Route::post('/cart/add', 'add')->name('cart.add');
+    Route::get('/cart', 'index')->name('cart.index');
+    Route::get('/cart/list', 'list')->name('cart.list');
+    Route::put('/cart/update/{id}', 'update')->name('cart.update');
+    Route::delete('/cart/remove/{id}', 'remove')->name('cart.remove');
+    Route::post('/cart/apply-coupon', 'applyCoupon')->name('cart.applyCoupon');
+    Route::get('/cart/checkout', 'order')->name('cart.checkout');
+    Route::post('/cart/remove-discount-code', 'removeDiscountCode')->name('cart.removeDiscountCode');
+});
